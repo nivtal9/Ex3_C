8100ef9b0fa467531526ce43a5c22187652f7195
nivtal9 <nivtal9@gmail.com>
2019-12-18 18:55:40 +0200

    change sortmain interface

--- isort_main.c
+++ isort_main.c
@@ -9,21 +9,9 @@ int main() {
         scanf(" %d", &temp_numb);
         *(i+arr) = temp_numb;
     }
-    printf("your arr before sort is:\n");
-    for (int j = 0; j < SIZE; j++) {
-        if (j == SIZE-1) {
-            printf("%d.\n", arr[j]);
-        } else {
-            printf("%d|", arr[j]);
-        }
-    }
         insertion_sort(arr, SIZE);
         printf("your arr after sort is:\n");
         for (int j = 0; j < SIZE; j++) {
-            if (j == SIZE-1) {
-                printf("%d.\n", arr[j]);
-            } else {
-                printf("%d|", arr[j]);
-            }
+		printf("%d,", arr[j]);
         }
     }
