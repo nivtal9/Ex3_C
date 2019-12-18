b3f7d3304d8bc1bfa686b4fe3634a34b017f55d6
nivtal9 <nivtal9@gmail.com>
2019-12-18 19:28:29 +0200

    fix problem with a/b command in txtfind

--- txtfind_main.c
+++ txtfind_main.c
@@ -5,7 +5,6 @@ int main() {
     char word[WORD];
     char command;
     getWord(word);
-    printf("please enter the command:(a/b)\n");
     scanf(" %c",&command);
     if(command=='a'){
         print_lines(word);
